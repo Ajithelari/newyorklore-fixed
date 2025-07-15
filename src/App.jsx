01632329772637482e9440ab71e2698e695657ec
Ajithelari <114493739+Ajithelari@users.noreply.github.com>
2025-07-15 14:52:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Stories from './pages/Stories';
 import Submit from './pages/Submit';
 
-function App() {
+export default function App() {
   return (
-    <Router>
+    <>
       <Header />
-      <main className="min-h-screen">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/stories" element={<Stories />} />
-          <Route path="/submit" element={<Submit />} />
-        </Routes>
-      </main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/stories" element={<Stories />} />
+        <Route path="/submit" element={<Submit />} />
+      </Routes>
       <Footer />
-    </Router>
+    </>
   );
 }
-
-export default App;
